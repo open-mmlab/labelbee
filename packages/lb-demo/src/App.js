c7756ef17fc88b44c9b80ebc363c29c54818221c
lijingchi <lijingchi1@sensetime.com>
2021-09-13 15:18:18 +0800

    feat: SENSEBEE-7425: lb-demo配置多步骤列表

--- packages/lb-demo/src/App.js
+++ packages/lb-demo/src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import Annotation from './Annotation';
+import Annotation from './components/Annotation';
 import { fileList as mockFileList, getMockResult } from './mock/index';
-import { getStepList } from './mock/taskConfig';
+import { getStepList, getDependStepList } from './mock/taskConfig';
 
 const getTool = () => new URLSearchParams(window.location.search).get('tool');
 
 const App = () => {
-  const stepList = getStepList(getTool());
+  const stepList = getDependStepList();
 
   const [fileList] = useState(
     mockFileList.map((url, i) => ({ id: i + 1, url, result: getMockResult(getTool()) })),
