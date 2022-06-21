0eb062e83247dd5782e55028a0773e90f04f7ae2
glenfiddish <edwinlee0927@hotmail.com>
2022-06-21 11:05:09 +0800

    feat: Add pointCloudView for entry of point cloud tools

---
+++ packages/lb-components/src/components/pointCloudView/PointCloud3DView.tsx
@@ -0,0 +1,26 @@
+/*
+ * @Author: Laoluo luozefeng@sensetime.com
+ * @Date: 2022-06-13 19:31:36
+ * @LastEditors: Laoluo luozefeng@sensetime.com
+ * @LastEditTime: 2022-06-13 20:07:03
+ */
+
+import { PointCloud } from '@labelbee/lb-annotation';
+import React, { useEffect, useRef } from 'react';
+const pointCloudID = 'LABELBEE-POINTCLOUD';
+
+const PointCloud3D = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const pointCloudRef = useRef<PointCloud>();
+
+  useEffect(() => {
+    if (ref.current) {
+      pointCloudRef.current = new PointCloud({ container: ref.current });
+      pointCloudRef.current.loadPCDFile('http://10.53.25.142:8001/1/000001.pcd');
+    }
+  }, []);
+
+  return <div id={pointCloudID} ref={ref} />;
+};
+
+export default PointCloud3D;
---
+++ packages/lb-components/src/components/pointCloudView/PointCloudTopView.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PointCloudTopView = () => {
+  return <div>PointCloudSideView</div>;
+};
+
+export default PointCloudTopView;
