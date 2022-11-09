9c41b67f246d33fb8843601e74091b120a3b1cf3
laoluo <ron.f.luo@gmail.com>
2022-11-09 19:32:29 +0800

    feat(line-tool): Add lineConfig type

--- packages/lb-annotation/src/types/tool/lineTool.d.ts
+++ packages/lb-annotation/src/types/tool/lineTool.d.ts
@@ -1,6 +1,7 @@
 declare interface ILinePoint extends IPoint {
   id: string;
   specialEdge?: boolean;
+  actual?: IPoint; // For internal use only
 }
 
 declare interface ILine {
@@ -13,4 +14,22 @@ declare interface ILine {
   attribute?: string;
   textAttribute?: string;
   isReference?: boolean;
+}
+
+declare interface ILineConfig extends IToolConfig {
+  edgeAdsorption: boolean;
+  outOfTarget: boolean;
+  copyBackwardResult: boolean;
+  isShowOrder: boolean;
+  lineType: ELineTypes;
+  lineColor: ELineColor;
+  attributeConfigurable: boolean;
+  attributeList: IInputList[];
+  textConfigurable: boolean;
+  textCheckType: ETextType;
+  customFormat: string;
+  lowerLimitPointNum: string;
+  upperLimitPointNum?: string;
+
+  showLineLength: boolean;
 }
