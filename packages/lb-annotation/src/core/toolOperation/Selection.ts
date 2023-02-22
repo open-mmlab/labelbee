3cb3c641a60f8b0eea28670c910e34bd325e26c4
lijingchi <lijingchi1@sensetime.com>
2023-02-22 16:23:58 +0800

    feat(multiple-select): Create class MultipleSelect

---
+++ packages/lb-annotation/src/core/toolOperation/Selection.ts
@@ -0,0 +1,126 @@
+import _ from 'lodash';
+import LineToolOperation from './LineToolOperation';
+import PointOperation from './pointOperation';
+import PolygonOperation from './polygonOperation';
+import { RectOperation } from './rectOperation';
+import CommonToolUtils from '../../utils/tool/CommonToolUtils';
+import { IPolygonData } from '@/types/tool/polygon';
+
+type ToolInstance = PointOperation | PolygonOperation | LineToolOperation | RectOperation;
+
+type SelectedID = string;
+type SelectedIDs = SelectedID[];
+type DataUnit = IPointUnit | IPolygonData | ILine | IRect;
+type DataList = Array<DataUnit>;
+
+class Selection {
+  private _selectedIDs: SelectedIDs;
+
+  private toolInstance: ToolInstance;
+
+  private stashDataList?: DataList;
+
+  constructor(toolInstance: ToolInstance) {
+    if (!toolInstance) {
+      console.error('MultipleSelect is require an  tool instance');
+    }
+
+    this._selectedIDs = [];
+    this.toolInstance = toolInstance;
+  }
+
+  get selectedIDs() {
+    return this._selectedIDs;
+  }
+
+  set selectedIDs(selectedIDs: SelectedIDs) {
+    this._selectedIDs = selectedIDs;
+    this.toolInstance.render();
+  }
+
+  get visibleDataList() {
+    const { dataList, attributeLockList, basicResult } = this.toolInstance;
+
+    const [showingDataList] = CommonToolUtils.getRenderResultList(
+      dataList,
+      CommonToolUtils.getSourceID(basicResult),
+      attributeLockList,
+    );
+
+    return showingDataList;
+  }
+
+  get dataList(): DataList {
+    return this.toolInstance.dataList;
+  }
+
+  /**
+   * Update selectedIDs:
+   * Remove selectedID when selectedIDs includes
+   * Append selectedID when selectedIDs not includes
+   * @param selectedID
+   */
+  public updateSelectedIDs(selectedID: SelectedID) {
+    if (this.selectedIDs.includes(selectedID)) {
+      this.selectedIDs = this.selectedIDs.filter((id) => id !== selectedID);
+    } else {
+      this.selectedIDs = [...this.selectedIDs, selectedID];
+    }
+  }
+
+  public updateSelectedGraphProps(updateList: DataList) {
+    const updatedDataList = _.cloneDeep(this.dataList).map((i) => {
+      const updatedData = updateList.find((s) => s.id === i.id);
+      if (updatedData) {
+        return _.merge(i, updatedData);
+      }
+
+      return i;
+    });
+
+    this.setResultAndRender(updatedDataList);
+  }
+
+  public selectAll() {
+    this.selectedIDs = this.visibleDataList.map((i) => i.id);
+    this.toolInstance.render();
+  }
+
+  public toStashDataList() {
+    if (this.selectedIDs.length > 0) {
+      this.stashDataList = _.cloneDeep(this.dataList.filter((i) => this.selectedIDs.includes(i.id)));
+    }
+
+    // todo: while not data, through an error
+  }
+
+  public toUnStashDataList() {
+    if (this.stashDataList) {
+      const _stashDataList = this.stashDataList;
+      this.stashDataList = [];
+      return _stashDataList;
+    }
+
+    return undefined;
+  }
+
+  public mergeStashData() {
+    const stashList = this.toUnStashDataList();
+    /**
+     * Merge current result and stashList
+     * https://stackoverflow.com/questions/38612972/how-to-merge-two-arrays-of-objects-by-id-using-lodash
+     */
+    const mergedDataList = _(this.dataList).keyBy('id').merge(_.keyBy(stashList, 'id')).values().value();
+    this.setResultAndRender(mergedDataList);
+  }
+
+  public setResultAndRender(dataList: DataList) {
+    // todo: Update tool instance and render
+  }
+
+  public isSelectedID(id: string) {
+    return this.selectedIDs.includes(id);
+  }
+}
+
+export default Selection;
