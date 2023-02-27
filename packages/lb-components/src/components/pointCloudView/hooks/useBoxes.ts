358085fc4ade279866dd0d0d46db85a6f59873bf
laoluo <ron.f.luo@gmail.com>
2023-02-27 15:36:28 +0800

    feat(pointcloud): Add ModalConfirm after copying same trackID

--- packages/lb-components/src/components/pointCloudView/hooks/useBoxes.ts
+++ packages/lb-components/src/components/pointCloudView/hooks/useBoxes.ts
@@ -1,7 +1,7 @@
-import { IPointCloudBoxList } from '@labelbee/lb-utils';
+import { IPointCloudBoxList, IPointCloudConfig } from '@labelbee/lb-utils';
 import { useCallback, useContext, useMemo, useState } from 'react';
 import _ from 'lodash';
-import { message } from 'antd';
+import { message, Modal } from 'antd';
 import { usePointCloudViews } from './usePointCloudViews';
 import { PointCloudContext } from '../PointCloudContext';
 import { useTranslation } from 'react-i18next';
@@ -9,17 +9,22 @@ import { useTranslation } from 'react-i18next';
 /**
  * Actions for selected boxes
  */
-export const useBoxes = () => {
-  const { selectedIDs, pointCloudBoxList, setPointCloudResult } = useContext(PointCloudContext);
+export const useBoxes = ({ config }: { config: IPointCloudConfig }) => {
+  const { selectedIDs, pointCloudBoxList, setPointCloudResult, syncAllViewPointCloudColor } =
+    useContext(PointCloudContext);
   const [copiedBoxes, setCopiedBoxes] = useState<IPointCloudBoxList>([]);
   const { pointCloudBoxListUpdated } = usePointCloudViews();
   const { t, i18n } = useTranslation();
 
-  // const hasDuplicateID = (checkBoxList: IPointCloudBoxList) => {
-  //   return pointCloudBoxList.some((item) => {
-  //     return checkBoxList.some((i) => i.id === item.id);
-  //   });
-  // };
+  const hasDuplicateID = (checkBoxList: IPointCloudBoxList) => {
+    if (config.trackConfigurable !== true) {
+      return false;
+    }
+
+    return pointCloudBoxList.some((item) => {
+      return checkBoxList.some((i) => i.trackID === item.trackID);
+    });
+  };
 
   const selectedBoxes = useMemo(() => {
     return pointCloudBoxList.filter((i) => selectedIDs.includes(i.id));
@@ -40,18 +45,43 @@ export const useBoxes = () => {
       return;
     }
 
-    // const hasDuplicate = hasDuplicateID(copiedBoxes);
-    const hasDuplicate = false; // Temporarily hide the duplicate check;
+    const hasDuplicate = hasDuplicateID(copiedBoxes);
+
+    const updatePointCloudResult = (newPointCloudBoxList: IPointCloudBoxList) => {
+      /** Paste succeed and empty */
+      setPointCloudResult(newPointCloudBoxList);
+      pointCloudBoxListUpdated?.(newPointCloudBoxList);
+      setCopiedBoxes([]);
+
+      // TODO: It need to update in global.
+      syncAllViewPointCloudColor(newPointCloudBoxList);
+    };
 
     if (hasDuplicate) {
-      message.error(t('HasDuplicateID'));
+      Modal.confirm({
+        title: t('HasDuplicateIDHeader'),
+        content: t('HasDuplicateIDMsg'),
+        onOk: () => {
+          /**
+           * Filter the same trackID in old-pointCloudBoxList.
+           */
+          const newPointCloudResult = pointCloudBoxList
+            .filter((v) => {
+              if (copiedBoxes.find((c) => c.trackID === v.trackID)) {
+                return false;
+              }
+              return true;
+            })
+            .concat(copiedBoxes);
+
+          updatePointCloudResult(newPointCloudResult);
+        },
+      });
     } else {
       /** Paste succeed and empty */
       const newPointCloudResult = [...pointCloudBoxList, ...copiedBoxes];
 
-      setPointCloudResult(newPointCloudResult);
-      pointCloudBoxListUpdated?.(newPointCloudResult);
-      setCopiedBoxes([]);
+      updatePointCloudResult(newPointCloudResult);
     }
   }, [copiedBoxes, pointCloudBoxList, i18n.language]);
 
