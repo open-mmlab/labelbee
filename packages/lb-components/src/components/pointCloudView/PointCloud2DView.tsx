2e3137d4474583f8e1e2d592bd02d7ecd40adda1
lixinghua.vendor <lixinghua.vendor@sensetime.com>
2023-08-16 10:38:22 +0800

    fix: 2d point cloud view style adjustments

--- packages/lb-components/src/components/pointCloudView/PointCloud2DView.tsx
+++ packages/lb-components/src/components/pointCloudView/PointCloud2DView.tsx
@@ -300,7 +300,7 @@ const PointCloud2DView = ({
             <PointCloudContainer
               className={classNames({
                 [getClassName('point-cloud-2d-container')]: true,
-                [getClassName('point-cloud-3d-containerZoom')]: showEnlarge,
+                [getClassName('point-cloud-container', 'zoom')]: showEnlarge,
               })}
               title={
                 <ContainerTitle
@@ -327,7 +327,6 @@ const PointCloud2DView = ({
                   currentData={currentData}
                   view2dData={item}
                   setSelectedID={setSelectedID}
-                  isHighlight={!showEnlarge}
                 />
               )}
             </PointCloudContainer>
