b00855fdf3e26de46091e30b76d7f75e1ae5a7bd
lihqi <455711093@qq.com>
2023-08-30 16:35:58 +0800

    feat: Add internationalized text

--- packages/lb-components/src/views/MainView/toolFooter/SwitchCuboidBoxIn2DView/index.tsx
+++ packages/lb-components/src/views/MainView/toolFooter/SwitchCuboidBoxIn2DView/index.tsx
@@ -1,9 +1,12 @@
-import React, { useContext } from 'react';
 import { Switch } from 'antd';
+import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import { PointCloudContext } from '@/components/pointCloudView/PointCloudContext';
 
 const SwitchCuboidBoxIn2DView = () => {
   const { cuboidBoxIn2DView, setCuboidBoxIn2DView } = useContext(PointCloudContext);
+  const { t } = useTranslation();
 
   const onChange = (checked: boolean) => {
     setCuboidBoxIn2DView(checked);
@@ -16,11 +19,11 @@ const SwitchCuboidBoxIn2DView = () => {
           marginRight: 10,
         }}
       >
-        图片标注物
+        {t('ImageLabels')}
       </span>
       <Switch
-        checkedChildren='3D框'
-        unCheckedChildren='2D框'
+        checkedChildren={t('3DRect')}
+        unCheckedChildren={t('2DRect')}
         checked={cuboidBoxIn2DView}
         onChange={onChange}
       />
