f760487359d478d72fda7b3f572c25fa73570083
lixinghua.vendor <lixinghua.vendor@sensetime.com>
2024-01-29 17:31:45 +0800

    feat: NLP tool type

--- packages/lb-components/src/components/NLPToolView/types.ts
+++ packages/lb-components/src/components/NLPToolView/types.ts
@@ -1,4 +1,5 @@
 import { IInfoList } from '@labelbee/lb-utils';
+import React from 'react';
 
 export interface IndicatorDetermine {
   label: string;
@@ -41,4 +42,45 @@ export interface INLPTextAnnotation {
   end: number;
   attribute: string;
   text: string;
+}
+
+export interface INLPInterval {
+  start: number;
+  end: number;
+  annotations?: INLPTextAnnotation[];
+  text: string;
+  attribute?: string;
+  id?: string;
+}
+
+export interface ISelectText {
+  id?: string;
+  start?: number;
+  end?: number;
+  text?: string;
+}
+
+export interface IRemarkLayer {
+  style: React.CSSProperties | undefined;
+  onClose: () => void;
+  submitData: ISelectText;
+}
+
+export interface IRemarkInterval {
+  start: number;
+  end: number;
+  remarkAnnotations?: IRemarkAnnotation[];
+  text: string;
+  attribute?: string;
+  id?: string;
+}
+
+
+export interface IRemarkAnnotation{
+  auditID: number,
+  text: string,
+  id: string,
+  start: number,
+  end: number,
+  uuid: string,
 }
