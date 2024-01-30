7ce997f1f1cd4704ad3a7dab15e43efe9dea3dd7
lijingchi <lijingchi1@sensetime.com>
2024-01-30 17:51:33 +0800

    feat(nlp): Remove useless shortcut

--- packages/lb-components/src/views/MainView/toolFooter/FooterTips/ToolHotKey/NLP/index.tsx
+++ packages/lb-components/src/views/MainView/toolFooter/FooterTips/ToolHotKey/NLP/index.tsx
@@ -1,12 +1,5 @@
 import { backward, forward } from '../common';
-import ForwardSvg from '@/assets/annotation/toolHotKeyIcon/icon_next_kj.svg';
 
-export const forwardWithEnter = {
-  name: 'Save',
-  icon: ForwardSvg,
-  shortCut: ['Ctrl', 'Enter'],
-};
-
-const NLPShortCutTable = [backward, forward, forwardWithEnter];
+const NLPShortCutTable = [backward, forward];
 
 export default NLPShortCutTable;
