507b2fade177976f66762216b606ace7ac703555
lixinghua.vendor <lixinghua.vendor@sensetime.com>
2024-01-30 17:58:42 +0800

    feat: Code review

---
+++ packages/lb-components/src/components/NLPToolView/textContent/remarkMask.tsx
@@ -0,0 +1,51 @@
+/**
+ * @file remark mask
+ * @author lixinghua <lixinghua_vendor@sensetime.com>
+ * @date 2024.01.30
+ */
+
+import React from 'react';
+import { IRemarkInterval } from '../types';
+
+export default ({
+  remarkSplitIntervals,
+  remark,
+}: {
+  remarkSplitIntervals: IRemarkInterval[];
+  remark?: any;
+}) => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        backgroundColor: 'transparent',
+        color: 'transparent',
+      }}
+    >
+      {remarkSplitIntervals.map((interval: IRemarkInterval, index: number) => {
+        const remarkAnnotation = _.last(interval.remarkAnnotations);
+        const highlight = interval?.remarkAnnotations?.find(
+          (i) => i?.auditID === remark.hoverAuditID,
+        );
+        const color = highlight ? '#ffc60a' : '#fcdf7e';
+        if (remarkAnnotation) {
+          return (
+            <span
+              style={{
+                borderBottom: `2px solid ${color}`,
+                padding: '2px 0px',
+              }}
+              id={remarkAnnotation?.id}
+              key={index}
+            >
+              {interval.text}
+            </span>
+          );
+        }
+        return <span key={index}>{interval.text}</span>;
+      })}
+    </div>
+  );
+};
