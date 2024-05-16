968814951540251cc47c5f893be1a89d019b33ca
lihqi <455711093@qq.com>
2024-05-16 14:14:08 +0800

    feat(lb-components): Add multi-select functionality in subAttributeList

--- packages/lb-components/src/components/subAttributeList/index.tsx
+++ packages/lb-components/src/components/subAttributeList/index.tsx
@@ -4,7 +4,7 @@
  * @createdate 2024-4-30
  */
 import React from 'react';
-import { Select, Divider } from 'antd';
+import { Select, Divider, Checkbox } from 'antd';
 import { useTranslation } from 'react-i18next';
 import AttributeList from '@/components/attributeList';
 
@@ -27,6 +27,24 @@ const SubAttributeList = (props: IProps) => {
   const { subAttributeList, setSubAttribute, getValue } = props;
   const { t } = useTranslation();
 
+  const setSubAttributeValue = (value: string, subAttribute: string | string[]) => {
+    if (Array.isArray(subAttribute)) {
+      setSubAttribute(value, subAttribute.join(';'));
+      return;
+    }
+    setSubAttribute(value, subAttribute);
+  };
+
+  const getInputValue = (subAttribute: IInputList) => {
+    const value = getValue(subAttribute);
+
+    if (subAttribute?.isMulti) {
+      return value ? value?.split(';') : [];
+    }
+
+    return value;
+  };
+
   return (
     <>
       {subAttributeList.map(
@@ -37,24 +55,41 @@ const SubAttributeList = (props: IProps) => {
                 {t('SubAttribute')}-{subAttribute.key}
               </div>
               {subAttribute.subSelected?.length < 5 ? (
-                <AttributeList
-                  list={subAttribute.subSelected.map((v: IInputList) => ({
-                    label: v.key,
-                    value: v.value,
-                  }))}
-                  selectedAttribute={getValue(subAttribute)}
-                  num='-'
-                  forbidColor={true}
-                  forbidDefault={true}
-                  attributeChanged={(value) => setSubAttribute(subAttribute.value, value)}
-                  style={{ marginBottom: 12 }}
-                />
+                subAttribute?.isMulti ? (
+                  <Checkbox.Group
+                    style={{
+                      padding: `0px 21px 17px 16px`,
+                    }}
+                    options={subAttribute.subSelected.map((v: IInputList) => ({
+                      label: v.key,
+                      value: v.value,
+                    }))}
+                    value={getInputValue(subAttribute) as string[]}
+                    onChange={(value) =>
+                      setSubAttributeValue(subAttribute.value, value as string[])
+                    }
+                  />
+                ) : (
+                  <AttributeList
+                    list={subAttribute.subSelected.map((v: IInputList) => ({
+                      label: v.key,
+                      value: v.value,
+                    }))}
+                    selectedAttribute={getValue(subAttribute)}
+                    num='-'
+                    forbidColor={true}
+                    forbidDefault={true}
+                    attributeChanged={(value) => setSubAttribute(subAttribute.value, value)}
+                    style={{ marginBottom: 12 }}
+                  />
+                )
               ) : (
                 <Select
                   style={{ margin: '0px 21px 17px 16px', width: '87%' }}
-                  value={getValue(subAttribute)}
+                  mode={subAttribute?.isMulti ? 'multiple' : undefined}
+                  value={getInputValue(subAttribute)}
                   placeholder={t('PleaseSelect')}
-                  onChange={(value) => setSubAttribute(subAttribute.value, value)}
+                  onChange={(value) => setSubAttributeValue(subAttribute.value, value)}
                   allowClear={true}
                 >
                   {subAttribute.subSelected.map((sub: any) => (
