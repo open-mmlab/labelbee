4828abc79c3babe57d618e238e4d2aef84be8128
lihqi <455711093@qq.com>
2024-09-27 15:20:06 +0800

    fix(lb-components): Add LLM MultiWheel textControl

--- packages/lb-components/src/store/LLMMultiWheel/index.ts
+++ packages/lb-components/src/store/LLMMultiWheel/index.ts
@@ -1,17 +1,18 @@
 import { EDataFormatType } from '@/constant';
 import { create } from 'zustand';
 import React from 'react';
+
+interface INewAnswerListMap {
+  [key: string]: string;
+}
+
 interface Store {
   dataFormatType: EDataFormatType;
   setDataFormatType: (dataFormatType: EDataFormatType) => void;
   selectedID: React.Key;
   setSelectedID: (selectedID: React.Key) => void;
-  highlightIDs: number[];
-  setHighlightIDs: (highlightIDs: number[]) => void;
-  selectedIDs: string[];
-  setSelectedIDs: (selectedIDs: string[]) => void;
-  rectRotateSensitivity: number;
-  setRectRotateSensitivity: (sensitivity: number) => void;
+  newAnswerListMap: INewAnswerListMap;
+  setNewAnswerListMap: (newAnswerListMap: INewAnswerListMap) => void;
 }
 
 const useLLMMultiWheelStore = create<Store>((set) => ({
@@ -19,16 +20,8 @@ const useLLMMultiWheelStore = create<Store>((set) => ({
   setDataFormatType: (dataFormatType) => set((state) => ({ dataFormatType })),
   selectedID: '',
   setSelectedID: (selectedID) => set((state) => ({ selectedID })),
-  highlightIDs: [],
-  setHighlightIDs: (highlightIDs) => set((state) => ({ highlightIDs })),
-  selectedIDs: [],
-  setSelectedIDs: (selectedIDs) =>
-    set((state) => {
-      return { selectedIDs };
-    }),
-  rectRotateSensitivity: 2,
-  setRectRotateSensitivity: (sensitivity) =>
-    set((state) => ({ rectRotateSensitivity: sensitivity })),
+  newAnswerListMap: {},
+  setNewAnswerListMap: (newAnswerListMap) => set((state) => ({ newAnswerListMap })),
 }));
 
 export default useLLMMultiWheelStore;
